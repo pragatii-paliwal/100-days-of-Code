5e3776c9e1a894344c1f0a1a4a50c8accaf8a100
Pragati Paliwal <Pragati.41234@stu.upes.ac.in>
2026-08-23 08:18:13 +0530

    Fixed Q2

--- Q2.C
+++ Q2.C
@@ -1,3 +1,22 @@
 // [Q2: Write a program to input two numbers and display their sum, difference, product, and quotient]
 
 #include <stdio.h>
+
+int main() {
+    float num1, num2, sum, difference, product, quotient;
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    sum = num1 + num2;
+    difference = num1 - num2;
+    product = num1 * num2;
+    quotient = num1 / num2;
+
+    printf("Sum: %f\n", sum);
+    printf("Difference: %f\n", difference);
+    printf("Product: %f\n", product);
+    printf("Quotient: %f\n", quotient);
+
+    return 0;
+}
